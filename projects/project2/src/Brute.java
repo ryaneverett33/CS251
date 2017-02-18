d4d6c5064b12c996a1d926ffe04f7831c47d089c
Ryan Everett <ryaneverett33@outlook.com>
2017-02-18 16:14:11 -0500

    Brute now works

--- projects/project2/src/Brute.java
+++ projects/project2/src/Brute.java
@@ -1,12 +1,14 @@
 /**
  * Created by Ryan on 2/10/2017.
  */
+import java.io.*;
 import java.util.*;
 public class Brute {
     // examines 4 points at a time and checks if they all lie on the same line segment, printing out any such line
     // segments to standard output and a file called "visualPoints.txt"
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //Input N /n x y
+        BufferedWriter writer = new BufferedWriter(new FileWriter("visualPoints.txt"));
         Point[] points;
         int arrCount = 0;           //Same as N
         int pointIndex = 0;
@@ -16,7 +18,7 @@ public class Brute {
         points = new Point[arrCount];
         while (StdIn.hasNextLine()) {
             String line = StdIn.readLine();
-            System.out.println("Read: " + line + ", length: " + line.length());
+            //System.out.println("Read: " + line + ", length: " + line.length());
             if (!line.equals("")) {
                 String firstX = line.substring(0,5);
                 firstX = firstX.trim();
@@ -31,13 +33,37 @@ public class Brute {
         }
         if (arrCount >= 4) {
             listSort(points);
-            //for (int i = 0; i < (arrCount - 3); i++) {
             for (int i = 0; i < (arrCount - 3); i++) {
-                if (Point.areCollinear(points[i], points[i+1], points[i+2], points[i+3])) {
-                    System.out.println("Colinear!");
+                for (int j = i+1; j < (arrCount - 2); j++) {
+                    for (int k = j+1; k < (arrCount - 1); k++) {
+                        for (int l = k+1; l < arrCount; l++) {
+                            /*if (i == 0) {
+                                Point.printSlopes(points[i], points[j], points[k], points[l]);
+                            }*/
+                            if (Point.areCollinear(points[i], points[j], points[k], points[l])) {
+                               // System.out.println(String.format("i: %d, j: %d, k: %d, l%d", i, j, k, l));
+                                /*System.out.println(String.format("(%d,%d),(%d,%d),(%d,%d),(%d,%d) are collinear",
+                                        points[i].x,points[i].y, points[j].x, points[j].y, points[k].x, points[k].y,
+                                        points[l].x,points[l].y));*/
+                                String pointStr = formatCollinear(points[i], points[j], points[k], points[l]);
+                                System.out.println(pointStr);
+                                writer.write(pointStr);
+                                writer.newLine();
+                            }
+                        }
+                    }
                 }
-                System.out.println("Point[" + i + "] = " + points[i].toString());
             }
+            writer.flush();
+            /*System.out.println("Test slope predictions");
+            Point q = points[1];
+            Point p = points[0];
+            float slope12 = (q.y - p.y) / (q.x - p.x);
+            System.out.println("Slope12: " + slope12);
+            System.out.println("Upper portion: " + (q.y - p.y));
+            System.out.println("Lower portion: " + (q.x - p.x));
+            float slope12CAST = (float)(q.y - p.y) / (float)(q.x - p.x);
+            System.out.println("Divisor when casted: " + slope12CAST);*/
         }
     }
     public static void listSort (Point[] list) {
@@ -60,5 +86,10 @@ public class Brute {
             return true;
         }
         return false;
+    }
+    public static String formatCollinear(Point a, Point b, Point c, Point d) {
+        //4:(0, 10000) -> (3000, 7000) -> (7000, 3000) -> (10000, 0)
+        return String.format("4:(%d, %d) -> (%d, %d) -> (%d, %d) -> (%d, %d)",
+                a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
     }
 }
--- projects/project2/src/Point.java
+++ projects/project2/src/Point.java
@@ -37,18 +37,51 @@ public class Point implements Comparable<Point>{
     // are the 4 points p, q, r, and s collinear?
     public static boolean areCollinear(Point p, Point q, Point r, Point s) {
         /* YOUR CODE HERE */
-        float slope1 = (q.y - p.y) / (q.x - p.x);
+        /*float slope1 = (q.y - p.y) / (q.x - p.x);
         float slope2 = (r.y - q.y) / (r.x - q.x);
         if (slope1 != slope2) {
             return false;
         }
         float slope3 = (s.y - r.y) / (s.x - r.x);
-        if (slope3 != slope2) {
+        if (slope3 != slope2 || slope3 != slope1) {
             return false;
         }
         else {
             return true;
+        }*/
+        float slope12 = (float)(q.y - p.y) / (float)(q.x - p.x);
+        float slope23 = (float)(r.y - q.y) / (float)(r.x - q.x);
+        if (slope12 != slope23) {
+            return false;
         }
+        float slope34 = (float)(s.y - r.y) / (float)(s.x - r.x);
+        float slope13 = (float)(r.y - p.y) / (float)(r.x - p.x);
+        if (slope34 != slope13) {
+            return false;
+        }
+        //since slope34 == slope13 && slope12 == slope23, then test if slope34 == slope34 then slope12,23,34,13 are =
+        if (slope23 != slope34) {
+            return false;
+        }
+        float slope14 = (float)(s.y - p.y) / (float)(s.x - p.x);
+        float slope24 = (float)(s.y - q.y) / (float)(s.x - q.x);
+        if (slope14 != slope24) {
+            return false;
+        }
+        if (slope13 != slope14) {
+            return false;
+        }
+        return true;
+    }
+    public static void printSlopes(Point p, Point q, Point r, Point s) {
+        float slope12 = (float)(q.y - p.y) / (float)(q.x - p.x);
+        float slope23 = (float)(r.y - q.y) / (float)(r.x - q.x);
+        float slope34 = (float)(s.y - r.y) / (float)(s.x - r.x);
+        float slope13 = (float)(r.y - p.y) / (float)(r.x - p.x);
+        float slope14 = (float)(s.y - p.y) / (float)(s.x - p.x);
+        float slope24 = (float)(s.y - q.y) / (float)(s.x - q.x);
+        System.out.println(String.format("12: %f, 23: %f, 34: %f, 13: %f, 14: %f, 24: %f",
+                slope12, slope23, slope34, slope13, slope14, slope24));
     }
 
     // is this point lexicographically smaller than that one?
