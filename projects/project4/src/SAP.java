0718d34590d9ad5aea15e4d5a9071943fb33e602
Ryan Everett <ryaneverett33@outlook.com>
2017-04-07 23:02:43 -0400

    WordNet working

--- projects/project4/src/SAP.java
+++ projects/project4/src/SAP.java
@@ -6,7 +6,7 @@ public class SAP {
     //don't use algs.jar
     Digraph graph;
     int lastV, lastW, lastLength, lastAncestor;
-    int lastVi, lastWi;
+    Iterable<Integer> lastVi, lastWi;
 
     // constructor
     public SAP(Digraph G) {
@@ -14,8 +14,8 @@ public class SAP {
         graph = G;
         lastV = -1;
         lastW = -1;
-        lastVi = -1;
-        lastWi = -1;
+        lastVi = null;
+        lastWi = null;
         lastAncestor = -1;
         lastLength = -1;
     }
@@ -33,9 +33,27 @@ public class SAP {
             //Consider the case where the inputs are backwards but still have the same length
             return lastLength;
         }
-        //do BreadFirstSearch
+        //do BreadthFirstSearch
         bfs(v, w);
 
+        return lastLength;
+    }
+    public int length (Iterable<Integer> v, Iterable<Integer> w) {
+        checkInput(v);
+        checkInput(w);
+
+        //Check if we've already calculated this
+        if (lastVi != null && lastWi != null) {
+            if (lastVi.equals(v) && lastWi.equals(w)) {
+                return lastLength;
+            }
+            if (lastVi.equals(w) && lastWi.equals(v)) {
+                return lastLength;
+            }
+        }
+
+        //do BreadthFirstSearch
+        bfs(v, w);
         return lastLength;
     }
     // return a common ancestor of v and w that participates in a shortest
@@ -59,9 +77,35 @@ public class SAP {
 
         return lastAncestor;
     }
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        //Similar to length(), check for valid inputs before using
+        checkInput(v);
+        checkInput(w);
+
+        //Check if we've already calculated this
+        if (lastVi != null && lastWi != null) {
+            if (lastVi.equals(v) && lastWi.equals(w)) {
+                return lastAncestor;
+            }
+            if (lastVi.equals(w) && lastWi.equals(v)) {
+                return lastAncestor;
+            }
+        }
+
+        bfs(v, w);
+
+        return lastAncestor;
+    }
     private void checkInput(int vertex) {
         if (vertex < 0 || vertex > (graph.V() - 1)) {
             throw new java.lang.IndexOutOfBoundsException();
+        }
+    }
+    private void checkInput(Iterable <Integer> vert) {
+        for (Integer v : vert) {
+            if (v < 0 || v > graph.V() - 1) {
+                throw new java.lang.IndexOutOfBoundsException();
+            }
         }
     }
     private void bfs(int v, int w) {
@@ -89,8 +133,38 @@ public class SAP {
         //Store computed values
         this.lastV = v;
         this.lastW = w;
-        this.lastVi = -1;
-        this.lastWi = -1;
+        this.lastVi = null;
+        this.lastWi = null;
+        this.lastLength = currLen;
+        this.lastAncestor = currAnc;
+    }
+    private void bfs(Iterable<Integer> v, Iterable<Integer> w) {
+        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(graph, v);
+        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(graph, w);
+        int currLen = Integer.MAX_VALUE;
+        int currAnc = -1;
+        for (int vert = 0; vert < graph.V(); vert++) {
+            if (bfsV.hasPathTo(vert) && bfsW.hasPathTo(vert)) {
+                //Split this up to make it easier for reading
+                int lenToVert = bfsV.distTo(vert);
+                lenToVert = lenToVert + bfsW.distTo(vert);
+                if (lenToVert < currLen) {
+                    //We found a shorter path, store it instead
+                    currLen = lenToVert;
+                    currAnc = vert;
+                }
+            }
+        }
+        //Check if we couldn't find any paths
+        if (currLen == Integer.MAX_VALUE) {
+            currLen = -1;
+        }
+
+        //Store computed values
+        this.lastV = -1;
+        this.lastW = -1;
+        this.lastVi = v;
+        this.lastWi = w;
         this.lastLength = currLen;
         this.lastAncestor = currAnc;
     }
@@ -98,7 +172,7 @@ public class SAP {
         //takes digraph input file as arg0 and digraph test input file as arg1
         //java SAP digraph1.txt digraph1.input
         if (args.length < 2) {
-            throw new Exception("Too many arguments");
+            throw new Exception("Too few arguments");
         }
         In graphIn = new In(args[0]);
         Digraph graph = new Digraph(graphIn);
--- projects/project4/src/WordNet.java
+++ projects/project4/src/WordNet.java
@@ -1,25 +1,92 @@
 /**
  * Created by Ryan on 4/7/2017.
  */
+import java.util.*;
 public class WordNet {
     //Don't use algs.jar
+    private Map<Integer, String> id2SynsetDefinition;
+    private Map<String, Bag<Integer>> synset2id;
+
+    private SAP sap;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
-        //TODO: Implement
+        id2SynsetDefinition = new HashMap<>();
+        synset2id = new HashMap<>();
+        //Load synsets
+        In synIn = new In(synsets);
+        while (synIn.hasNextLine()) {
+            //synset is a csv file
+            //id, word & synonyms, definition
+            String currLine = synIn.readLine();
+            String[] currSplit = currLine.split(",");
+            int id = Integer.parseInt(currSplit[0]);
+            String definition = currSplit[1] + ',' + currSplit[2];
+            id2SynsetDefinition.put(id, definition);
+
+            String synonyms[] = currSplit[1].split(" ");
+            for (int i = 0; i < synonyms.length; i++) {
+                Bag<Integer> bag = synset2id.get(synonyms[i]);
+                if (bag == null) {
+                    Bag<Integer> newBag = new Bag<>();
+                    newBag.add(id);
+                    synset2id.put(synonyms[i], newBag);
+                }
+                else {
+                    bag.add(id);
+                }
+            }
+        }
+        In hyperIn = new In(hypernyms);
+        Digraph graph = new Digraph(id2SynsetDefinition.size());
+        while (hyperIn.hasNextLine()) {
+            //csv #id,
+            String currLine = hyperIn.readLine();
+            String[] values = currLine.split(",");
+            int rootEdge = Integer.parseInt(values[0]);
+            if (rootEdge == 81426) {
+                continue;
+            }
+            for (int i = 0; i < values.length; i++) {
+                graph.addEdge(rootEdge, Integer.parseInt(values[i]));
+            }
+        }
+        sap = new SAP(graph);
     }
     // is the word a WordNet noun? This can be used to search for existing
 // nouns at the beginning of the printSap method
     public boolean isNoun(String word) {
-        //TODO: Implement
-        return false;
+        return synset2id.containsKey(word);
     }
     // print the synset (second field of synsets.txt) that is the common ancestor
 // of nounA and nounB in a shortest ancestral path as well as the length of the path,
 // following this format: "sap<space>=<space><number>,<space>ancestor<space>=<space><synsettext>"
 // If no such path exists the sap should contain -1 and ancestor should say "null"
 // This method should use the previously defined SAP datatype
-    public void printSap(String nounA, String nounB) {
-        //TODO: Implement
+    public void printSap(String nounA, String nounB) throws Exception {
+        if (!isNoun(nounA) || !isNoun(nounB)) {
+            //throw new java.lang.Exception("WordNet does not contains a requested noun");
+            System.out.println("sap = -1, ancestor = null");
+            return;
+        }
+        int id = sap.ancestor(synset2id.get(nounA), synset2id.get(nounB));
+        String definition = id2SynsetDefinition.get(id);
+        //strip word from definition
+        String defWord = definition.substring(0, definition.indexOf(','));
+        int length = sap.length(synset2id.get(nounA), synset2id.get(nounB));
+        System.out.printf("sap = %d, ancestor = %s\n", length, defWord);
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3) {
+            throw new Exception("Too few arguments");
+        }
+        WordNet net = new WordNet(args[0], args[1]);
+        In wordIn = new In(args[2]);
+        while (wordIn.hasNextLine()) {
+            String currLine = wordIn.readLine();
+            String[] currSplit = currLine.split(" ");
+            net.printSap(currSplit[0], currSplit[1]);
+        }
     }
 }
